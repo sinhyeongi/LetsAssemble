ce33bf28d07e0047d9ec4ad5dc236d8c96dfeaf1
신현기 <sinhg1221@daum.net>
2024-04-02 11:09:46 +0900

    add Project

---
+++ LetsAssemble/src/main/java/com/la/letsassemble/LetsAssembleApplication.java
@@ -0,0 +1,13 @@
+package com.la.letsassemble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsAssembleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsAssembleApplication.class, args);
+    }
+
+}
---
+++ LetsAssemble/src/test/java/com/la/letsassemble/LetsAssembleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.la.letsassemble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsAssembleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
