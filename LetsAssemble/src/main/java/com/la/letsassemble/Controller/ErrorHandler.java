766357fbf1ad5cd30e9195a116a911f71ddb6846
JunYoungKim-kjy <firstkjy1123@gmail.com>
2024-04-04 18:33:30 +0900

    update : signup page 업데이트 및 이메일 기능

--- LetsAssemble/src/main/java/com/la/letsassemble/Controller/ErrorHandler.java
+++ LetsAssemble/src/main/java/com/la/letsassemble/Controller/ErrorHandler.java
@@ -2,14 +2,17 @@ package com.la.letsassemble.Controller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+
 @Controller
 public class ErrorHandler implements ErrorController {
     @RequestMapping("/error")
-    public String handleNotFoundException(HttpServletRequest request){
+    public String handleNotFoundException(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Object o = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         if(o != null){
             int code = Integer.parseInt(o.toString());
@@ -17,6 +20,9 @@ public class ErrorHandler implements ErrorController {
                 return "ERROR/404";
             }else if(code == 403){
                 return "ERROR/403";
+            }else if(code == 400){
+                response.getWriter().print("err");
+                return null;
             }
         }
         return "ERROR/err";
--- LetsAssemble/src/main/java/com/la/letsassemble/Controller/UserController.java
+++ LetsAssemble/src/main/java/com/la/letsassemble/Controller/UserController.java
@@ -1,16 +1,28 @@
 package com.la.letsassemble.Controller;
 
+import com.la.letsassemble.Entity.Users;
+import com.la.letsassemble.Service.UsersService;
+import com.la.letsassemble.dto.EmailRequestDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final UsersService usersService;
 
     @GetMapping("")
     public String signupForm(){
         return "signup";
+    }
+
+    @GetMapping("/validate")
+    public @ResponseBody boolean emailValidate(@RequestParam String email){
+        System.out.println("param = "+ email);
+        return usersService.findByEmail(email).isPresent();
     }
 
 }
