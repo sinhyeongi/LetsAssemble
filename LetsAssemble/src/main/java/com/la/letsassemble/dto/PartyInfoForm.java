8ade1d9222c7ddbdcfd7080eb8656c55eec6ed8a
JunYoungKim-kjy <firstkjy1123@gmail.com>
2024-04-18 21:23:41 +0900

    ignore

---
+++ LetsAssemble/src/main/java/com/la/letsassemble/dto/PartyInfoForm.java
@@ -0,0 +1,10 @@
+package com.la.letsassemble.dto;
+
+import lombok.Data;
+
+@Data
+public class PartyInfoForm{
+    private String state;
+    private Boolean isBlack;
+    private String nickname;
+}
