abe3e7cf5aa82f6d1d98cba121803848d33ce494
신현기 <sinhg1221@daum.net>
2024-04-18 15:53:00 +0900

    test: server
    server test 완료

--- LetsAssemble/src/main/java/com/la/letsassemble/config/WebSocketConfig.java
+++ LetsAssemble/src/main/java/com/la/letsassemble/config/WebSocketConfig.java
@@ -37,7 +37,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override //
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:0880")
+                .setAllowedOrigins("http://localhost:8080")
                 .withSockJS()
                 .setInterceptors(httpSessionHandshakeInterceptor());
     }
